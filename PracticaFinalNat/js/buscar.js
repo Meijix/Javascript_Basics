5b62859e953ce5865c5f83f9a24840a452627fd4
Meijix <nmejia30@ciencias.unam.mx>
2024-12-20 16:04:09 -0600

    Improve search functionality

--- PracticaFinalNat/js/buscar.js
+++ PracticaFinalNat/js/buscar.js
@@ -4,15 +4,36 @@ $(document).ready(function(){
 	var busqueda = $("#busqueda");
 
 	$("#btn-buscar").on("click", function(){
+		//verificar que la palabra no esté vacía
+		if(busqueda.val() != ""){
 
-		const palabra = $('#busqueda').val();
-		console.log('Palabra a buscar: '+palabra);
-		alert('Vamos a buscar: '+palabra);
-		/*PONGA AQUI SU CÓDIGO*/
-		
-
+			//Obtener la palabra a buscar
+			const palabra = $('#busqueda').val();
+			console.log('Palabra a buscar: ' + palabra);
+			alert('Vamos a buscar: ' + palabra);
+			//Usar AJAX para buscar la palabra
+			$.ajax({
+				type: "GET",
+				url: `https://api.themoviedb.org/3/search/movie?certification_country=MX&language=es&api_key=3356865d41894a2fa9bfa84b2b5f59bb&query=${encodeURIComponent(palabra)}`,
+				dataType: "json",
+				success: function(data){
+					//console.log(data);
+					$("#miLista").empty();
+					$.each(data.results, function(index, item){
+						$("#miLista").append("<li>" + item.title + "</li>");
+						//mostrar imagen
+						$("#miLista").append("<img class='card-img' src='https://image.tmdb.org/t/p/w500" + item.poster_path + "' />");
+						//mostrar descripción
+						$("#miLista").append("<p class='description'>" + item.overview + "</p>");
+					});
+				},
+				error: function(){
+					alert('Error al buscar la palabra');
+				}
+			});
+		} else {
+			alert('Debes ingresar una palabra para buscar');
+		}
 	});
-
 });
-
 
