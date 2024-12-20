0eac78f2286cff62d0aaad679f153f93e91fb0e2
Meijix <nmejia30@ciencias.unam.mx>
2024-12-20 14:58:20 -0600

    Add Practica Final files

---
+++ PracticaFinalNat/js/buscar.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	var miLista = $("#miLista");
+	var busqueda = $("#busqueda");
+
+	$("#btn-buscar").on("click", function(){
+
+		const palabra = $('#busqueda').val();
+		console.log('Palabra a buscar: '+palabra);
+		alert('Vamos a buscar: '+palabra);
+		/*PONGA AQUI SU CÓDIGO*/
+		
+
+	});
+
+});
+
+
